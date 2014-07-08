e7672ae8a32a6b8308c3925995d0ad347e0c1d8e
David Megginson <david.megginson@megginson.com>
2014-07-08 16:56:51 -0400

    Strip out non-allowed search characters.
    Closes #10

--- lib/controllers/SearchController.php
+++ lib/controllers/SearchController.php
@@ -4,16 +4,27 @@ class SearchController extends AbstractController {
 
   function doGET(HttpRequest $request, HttpResponse $response) {
 
+    // HTTP GET parameters
     $q = $request->get('q');
     $source_ident = $request->get('source');
     $code_code = $request->get('code');
     $user_ident = $request->get('user');
 
-    // Values for select lists
+    // Clean up query string to match search syntax
+    // TODO - figure out how to make more Google-like
+    $q = preg_replace("/[^a-zA-Z0-9]+/", "+", $q);
+    $q = preg_replace("/^\\++/", "", $q);
+    $q = preg_replace("/\\++$/", "", $q);
+
+    // Options for select lists
     $codes = $this->doQuery('select * from code order by code');
     $sources = $this->doQuery('select * from source order by ident');
     $users = $this->doQuery('select * from usr order by ident');
 
+    //
+    // Construct the search query incrementally, depending on what facets
+    // the user has specified
+    //
     $params = array($q);
     $frag = "from value_view V join latest_import_view LI on V.import=LI.id where to_tsvector('english', V.value) @@ to_tsquery('english', ?)";
 
@@ -32,23 +43,38 @@ class SearchController extends AbstractController {
       $frag .= " and V.usr_ident=?";
     }
 
-    array_unshift($params, "select V.source_ident, V.source_name, V.dataset_ident, V.dataset_name, V.code_code, V.code_name, V.value, V.usr_ident, V.usr_name, count(V.id) as row_count $frag group by V.source_ident, V.source_name, V.dataset_ident, V.dataset_name, V.code_code, V.code_name, V.value, V.usr_ident, V.usr_name order by row_count desc");
+    // Crazy grouping statement to get aggregate totals for datasets
+    $sql = "select V.source_ident, V.source_name, " .
+      "V.dataset_ident, V.dataset_name, " .
+      "V.code_code, V.code_name, " .
+      "V.value, V.usr_ident, V.usr_name, " .
+      "count(V.id) as row_count " .
+      $frag . ' ' .
+      "group by V.source_ident, V.source_name, " .
+      "V.dataset_ident, V.dataset_name, " .
+      "V.code_code, V.code_name, " .
+      "V.value, V.usr_ident, V.usr_name " .
+      "order by row_count desc";
+    array_unshift($params, $sql);
     $values = call_user_method_array('doQuery', $this, $params);
 
     array_shift($params);
     array_unshift($params, "select count(V.*) $frag");
     $result_count = call_user_method_array('doQuery', $this, $params)->fetchColumn();
 
+    // Set up the template parameters
+    $response->setParameter('q', $q);
+
     $response->setParameter('codes', $codes);
     $response->setParameter('sources', $sources);
     $response->setParameter('users', $users);
 
-    $response->setParameter('q', $q);
     $response->setParameter('source_ident', $source_ident);
     $response->setParameter('code_code', $code_code);
     $response->setParameter('user_ident', $user_ident);
     $response->setParameter('values', $values);
     $response->setParameter('result_count', $result_count);
+
     $response->setTemplate('search');
   }
 
