a848d75b9cf03b0de960d838a6b5a03700cf3755
David Megginson <david.megginson@megginson.com>
2014-07-22 19:07:13 -0400

    Optimised input controller to return results quickly.

--- lib/controllers/ImportAnalysisController.php
+++ lib/controllers/ImportAnalysisController.php
@@ -28,36 +28,33 @@ class ImportAnalysisController extends AbstractController {
       'sector' => 'sector',
       'org' => 'org',
     );
-    list($sql_filter, $active_filters) = self::process_filters($request, $filter_map);
+    list($sql_filter, $active_filters) = self::process_filters($request, $import->id, $filter_map);
 
     //
     // Metrics
     //
-    $total = $this->doQuery(
-      'select count(distinct R.row) from row R' . $sql_filter .
-      ' where R.import=?',
-      $import->id
-    )->fetchColumn();
+
+    $total = $this->get_total_count($sql_filter);
 
     // Get the preview counts
-    if (!$active_filters['country']) {
-      $country_count = $this->get_value_count($import->id, 'country', $sql_filter);
-      $countries = $this->get_value_preview($import->id, 'country', $sql_filter);
+    if (!@$active_filters['country']) {
+      $country_count = $this->get_value_count('country', $sql_filter);
+      $countries = $this->get_value_preview('country', $sql_filter);
     }
 
-    if (!$active_filters['adm1'] && !@$country_count) {
-      $adm1_count = $this->get_value_count($import->id, 'adm1', $sql_filter);
-      $adm1s = $this->get_value_preview($import->id, 'adm1', $sql_filter);
+    if (!@$active_filters['adm1'] && !@$country_count) {
+      $adm1_count = $this->get_value_count('adm1', $sql_filter);
+      $adm1s = $this->get_value_preview('adm1', $sql_filter);
     }
 
-    if (!$active_filters['sector']) {
-      $sector_count = $this->get_value_count($import->id, 'sector', $sql_filter);
-      $sectors = $this->get_value_preview($import->id, 'sector', $sql_filter);
+    if (!@$active_filters['sector']) {
+      $sector_count = $this->get_value_count('sector', $sql_filter);
+      $sectors = $this->get_value_preview('sector', $sql_filter);
     }
 
-    if (!$active_filters['org']) {
-      $org_count = $this->get_value_count($import->id, 'org', $sql_filter);
-      $orgs = $this->get_value_preview($import->id, 'org', $sql_filter);
+    if (!@$active_filters['org']) {
+      $org_count = $this->get_value_count('org', $sql_filter);
+      $orgs = $this->get_value_preview('org', $sql_filter);
     }
 
     //
@@ -91,6 +88,15 @@ class ImportAnalysisController extends AbstractController {
   }
 
   /**
+   * Count the total number of rows matching the filter.
+   */
+  private function get_total_count($sql_filter) {
+    return 0 + $this->doQuery(
+      'select count(distinct R.row) from (' . $sql_filter . ') R'
+    )->fetchColumn();
+  }
+
+  /**
    * Count the number of distinct values for a column.
    *
    * @param $import The import identifier (long integer).
@@ -98,17 +104,17 @@ class ImportAnalysisController extends AbstractController {
    * @param $sql_filter The SQL filter subquery (optional).
    * @return The number of distinct values (integer).
    */
-  private function get_value_count($import_id, $code, $sql_filter = '') {
+  private function get_value_count($code, $sql_filter) {
     return 0 + $this->doQuery(
-      'select count(distinct R.value) as count' .
-      ' from value_view R ' . $sql_filter .
-      ' where R.import=? and R.code_code=?',
-      $import_id, $code
+      'select count(distinct V.value) as count' .
+      ' from value_view V ' .
+      ' where V.code_code=? and V.row in ' . $sql_filter,
+      $code
     )->fetchColumn();
   }
 
   /**
-   * Get the top 5 values for a column.
+   * Get the top values for a column.
    *
    * The result objects will have a "count" property with the number
    * of matches, and a "value" property with the cell value.
@@ -118,14 +124,14 @@ class ImportAnalysisController extends AbstractController {
    * @param $sql_filter The SQL filter subquery (optional).
    * @return A list of result objects.
    */
-  private function get_value_preview($import_id, $code, $sql_filter = '') {
+  private function get_value_preview($code, $sql_filter) {
     return $this->doQuery(
-      "select R.value, count(distinct R.row) as count " .
-      ' from value_view R' . $sql_filter .
-      ' where R.import=? and R.code_code=?' .
-      ' group by R.value' .
-      " order by count(distinct R.row) desc, R.value",
-      $import_id, $code
+      'select V.value, count(distinct V.row) as count ' .
+      ' from value_view V' .
+      ' where V.code_code=? and V.row in ' . $sql_filter .
+      ' group by V.value' .
+      ' order by count(distinct V.row) desc, V.value',
+      $code
     );
   }
 
@@ -136,7 +142,7 @@ class ImportAnalysisController extends AbstractController {
    * @param $filter_map An associative array of request parameters mapped to HXL codes.
    * @return A list containing the SQL fragment and an array of the actual filters selected.
    */
-  private static function process_filters(HttpRequest $request, $filter_map) {
+  private static function process_filters(HttpRequest $request, $import_id, $filter_map) {
 
     // Return values
     $sql_filter = '';
@@ -152,11 +158,25 @@ class ImportAnalysisController extends AbstractController {
           $value = array_pop($value);
         }
         $active_filters[$http] = $value;
+
+        // Different treatment for the first one
+        if ($n == 1) {
+          $sql_filter = 'select V1.row from value_view V1';
+          $where_clause = sprintf(" where V1.import=%d and V1.code_code='%s' and V1.value='%s'", $import_id, $hxl, $value);
+        } else {
         $sql_filter .= sprintf(
-          ' join value_view V%d on R.row=V%d.row and V%d.code_code=\'%s\' and V%d.value=\'%s\'',
+          ' join value_view V%d on V1.row=V%d.row and V%d.code_code=\'%s\' and V%d.value=\'%s\'',
           $n, $n, $n, $hxl, $n, $value
         );
+        }
       }
+    }
+
+    if ($sql_filter) {
+      $sql_filter = sprintf('(%s %s)', $sql_filter, $where_clause);
+    } else {
+      // count all rows
+      $sql_filter = sprintf('(select row from row where import=%d)', $import_id);
     }
 
     // Return the results
