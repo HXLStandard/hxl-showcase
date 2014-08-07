421ff00814b1926a28a974450e222155068a8503
David Megginson <david.megginson@megginson.com>
2014-08-07 10:31:15 -0400

    Test JSON.

--- lib/controllers/StatsController.php
+++ lib/controllers/StatsController.php
@@ -66,6 +66,7 @@ class StatsController extends AbstractController {
     print "[";
     $is_first = true;
     foreach ($stats as $stat) {
+      print_r($stat);exit;
       if ($is_first) {
         $is_first = false;
       } else {
